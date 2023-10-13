bd79fd1b1f14222bdd9a49ee50e41f354ae68a95
Ilyes Soumar Djouma <isoumardjouma@reseau.free.fr>
2023-10-14 00:23:45 +0200

    code review

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 //DOM ELEMENTS
 const vision = document.getElementById("vision");
 const sword = document.getElementById("sword");
+const shield = document.getElementById("shield");
 const btnCool = document.getElementById("btn-cool");
 const screenText = document.querySelector(".screen-text");
-
-let msgCombat = "";
-
 
 sword.addEventListener("click", attack)
 
 function attack (){
- msgCombat.innerHTML = "Lancer une attaque normale!";
+const textCombat = document.createElement("p");
+screenText.appendChild(textCombat);
+textCombat.innerText = "Normal hit on target.";
+screenText.scrollTop = screenText.scrollHeight;
 }
 
+shield.addEventListener("click", defense)
 
-screenText
+function defense (){
+  const textCombat = document.createElement("p");
+  screenText.appendChild(textCombat);
+  textCombat.innerText = "Defensive stance, you will reduce the incoming damage by 30%.";
+  screenText.scrollTop = screenText.scrollHeight;
+  }
